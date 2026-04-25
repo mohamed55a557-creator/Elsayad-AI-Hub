f95763d5146ccdc2b870b9fa736d72a9f6c9d715
mohamed55a557-creator <mohamed55a557@gmail.com>
2026-04-25 21:13:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if prompt := st.chat_input("اسأل الصياد..."):
         }
         # هنا بنختار الموديل المجاني من OpenRouter
         data = {
-            "model": "google/gemma-2-9b-it:free", 
+            "model": "google/gemma-4-26b-a4b-it:free", 
             "messages": st.session_state.messages
         }
         
